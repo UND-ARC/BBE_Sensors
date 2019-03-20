7decea05e380ca42b192405c0aed20ccbfd2c89b
Demo User <debian@beaglebone.localdomain>
2019-03-20 23:40:14 +0000

    Started commenting some of the code in simple_read.c

--- sc_mpu6050/simple_read.c
+++ sc_mpu6050/simple_read.c
@@ -1,68 +1,73 @@
-#include "/usr/include/stdio.h" //<stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <sys/select.h>
-#include <pthread.h>
-#include <string.h>
-#include <ctype.h>
+//Simple read function for the accel/gyro on the Beaglebone Enhanced
+//Comments added by ARC at the University of North Dakota
+
+#include <stdio.h> 	//usr/include/stdio.h
+#include <stdlib.h>	//.
+#include <unistd.h>	//.
+#include <sys/types.h>	//.
+#include <sys/stat.h>	//.
+#include <fcntl.h>	//.
+#include <sys/select.h>	//.
+#include <pthread.h>	//.
+#include <string.h>	//.
+#include <ctype.h>	//.
 
 #include "types.h"
 #include "sysfs_helper.h"
 
-static pthread_t agread_id = NULL;
+static pthread_t agread_id = NULL;		//
 
-static char *dev_dir_name, *buf_dir_name;
-static bool reader_runnning = false;
-static bool ag_pass = false;
-static int ax, ay, az;
-static int gx, gy, gz;
-static int cax, cay, caz;
-static int cgx, cgy, cgz;
-static float fin_anglvel_scale, fin_accel_scale;
-static float fx, fy, fz;
-static float fgx, fgy, fgz;
+//statics
+static char *dev_dir_name, *buf_dir_name;	//device directory name, buffer directory name
+static bool reader_runnning = false;		//Vestigial variable?
+static bool ag_pass = false;			//
+static int ax, ay, az;				//Accel x, y, z
+static int gx, gy, gz;				//Gyro x, y, z
+static int cax, cay, caz;			//Calibration(?)accel x, y, z
+static int cgx, cgy, cgz;			//Calibration(?)gyro x, y, z
+static float fin_anglvel_scale, fin_accel_scale;//final(?) accel/velocity scale
+static float fx, fy, fz;			//final(?) x, y, z
+static float fgx, fgy, fgz;			//final(?) gyro(?) x, y, z
 
-const char *iio_dir = "/sys/bus/iio/devices/";
+//constants
+const char *iio_dir = "/sys/bus/iio/devices/";	//iio directory location
 
-unsigned long timedelay = 100000;
-unsigned long buf_len = 128;
+//unsigneds
+unsigned long timedelay = 100000;		//Vestigial variable?
+unsigned long buf_len = 128;			//Why is this an unsigned long?
 
-int ret, c, i;
-int fp;
-int err;
+//normal
+int ret, c, i;			//
+int fp;				//
+int err;			//Also declared in main
+int num_channels;		//
+char *trigger_name = NULL;	//
+int datardytrigger = 1;		//Data ready trigger(?)
+char *data;			//The only self-documenting variable in this entire program
+int read_size;			//
+int dev_num, trig_num;		//
+char *buffer_access;		//
+int scan_size;			//
+int noevents = 0;		//
+int p_event = 0, nodmp = 0;	//
+char *dummy;			//Is this a dummy variable?
+char chip_name[10];		//
+char device_name[10];		//Device name; used to decide which iio:device we are using
+char sysfs[100];		//
 
 
-int num_channels;
-char *trigger_name = NULL;
-
-int datardytrigger = 1;
-char *data;
-int read_size;
-int dev_num, trig_num;
-char *buffer_access;
-int scan_size;
-int noevents = 0;
-int p_event = 0, nodmp = 0;
-char *dummy;
-char chip_name[10];
-char device_name[10];
-char sysfs[100];
-
-
+//???
 int read_sysfs_posint(char *filename, char *basedir, int* val) {
-    int ret = 0;
-    int len = 0;
-    FILE *sysfsfp;
-    char buff[20] = {0};
-    char *endptr;
-    int filedesc;
+    int ret = 0;	//Originally declared globally
+    int len = 0;	//^           ^              ^
+    FILE *sysfsfp;	//
+    char buff[20] = {0};//
+    char *endptr;	//
+    int filedesc;	//
     long newval = 0;
 
     if(val != NULL){
-		char *temp = malloc(strlen(basedir) + strlen(filename) + 2);
+		char *temp = malloc(strlen(basedir) + strlen(filename));
 		if (temp == NULL) {
 			DEBUG_MSG("Memory allocation failed");
 			return -ENOMEM;
@@ -76,26 +81,32 @@ int read_sysfs_posint(char *filename, char *basedir, int* val) {
 
 		filedesc = open( temp, O_RDONLY );
 		len = 20;
-		len = read( filedesc, buff, len ); /* there was data to read */
-	    len=len;
-		close( filedesc );
-		newval = strtol(buff, &endptr, 10);
-		*val = (int)newval;
-		fclose(sysfsfp);
-		error_free: free(temp);
+		len = read( filedesc, buff, len ); 	//there was data to read
+	    	//len=len;				//This line is literally useless, so we commented it out
+		close(filedesc);			//
+		newval = strtol(buff, &endptr, 10);	//
+		*val = (int)newval;			//
+		fclose(sysfsfp);			//
+		error_free: free(temp);			//
     }
     return ret;
 }
 
+//
 int read_sysfs_float(char *filename, char *basedir, float *val) {
-    int ret = 0;
-    int len = 0;
-    FILE *sysfsfp;
+    int ret = 0;//Also declared globally
+    int len = 0;//^        ^           ^
+    FILE *sysfsfp;	//Some kind of file
+
+    //Allocate memory for temp
     char *temp = malloc(strlen(basedir) + strlen(filename) + 2);
+
+    //Make sure we allocated memory properly
     if (temp == NULL) {
         DEBUG_MSG("Memory allocation failed");
         return -ENOMEM;
     }
+
     sprintf(temp, "%s/%s", basedir, filename);
     sysfsfp = fopen(temp, "r");
     if (sysfsfp == NULL) {
@@ -103,23 +114,27 @@ int read_sysfs_float(char *filename, char *basedir, float *val) {
         goto error_free;
     }
     len = fscanf(sysfsfp, "%f\n", val);
-    len=len;
+    //len=len;		//reddit.com/r/badcode
     fclose(sysfsfp);
     error_free: free(temp);
     return ret;
 }
 
 int read_sysfs_string(char *filename, char *basedir, char* val) {
-    int ret = 0;
-    int len = 0;
+    int ret = 0;//Again, redeclared from global
+    int len = 0;//^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
     FILE *sysfsfp;
 
     if(val != NULL){
+		//Allocate some memory and verify that it was allocated properly
 		char *temp = malloc(strlen(basedir) + strlen(filename) + 2);
 		if (temp == NULL) {
 			DEBUG_MSG("Memory allocation failed");
 			return -ENOMEM;
 		}
+
+		
 		sprintf(temp, "%s/%s", basedir, filename);
 		sysfsfp = fopen(temp, "r");
 		if (sysfsfp == NULL) {
@@ -135,8 +150,8 @@ int read_sysfs_string(char *filename, char *basedir, char* val) {
 }
 
 /**************************************************************************
- * Function Name  : read_thread
- * Parameter      : none
+ * Function Name  : ag_read_thread (changed from read_thread)
+ * Parameter      : none(there's an argument here)
  * Description    : Thread to supply gyro and accel data over a socket
  * Return         : None
  * Scope          : GLOBAL
@@ -145,7 +160,7 @@ int read_sysfs_string(char *filename, char *basedir, char* val) {
 void* ag_read_thread(void* arg) {
 	int loop_count = 0;
 	read_sysfs_float("in_anglvel_scale", dev_dir_name, &fin_anglvel_scale);
-    read_sysfs_float("in_accel_scale", dev_dir_name, &fin_accel_scale);
+    	read_sysfs_float("in_accel_scale", dev_dir_name, &fin_accel_scale);
 
     //Calibration initialisation
     cax = 0;
@@ -241,7 +256,7 @@ void* ag_read_thread(void* arg) {
             //DEBUG_MSG("%7.2f, %7.2f, %7.2f\n", fz, fx, fy);
 
             //10ms sample rate
-            usleep(10000);
+            usleep(10);
         }
     }
 error_out:
@@ -253,10 +268,10 @@ error_out:
 
 
 
-
+//
 int discover (void)
 {
-	int ret = 0;
+	int ret = 0;	//Return variable, declared globally
 	int asret = 0;
 
 //    DEBUG_MSG("Getting sysfs path\n");
@@ -280,24 +295,26 @@ int discover (void)
 //    DEBUG_MSG("device name: %s\n", device_name);
 
     /* Find the device requested */
-    dev_num = find_type_by_name(device_name, "iio:device");
+    dev_num = find_type_by_name(device_name, "iio:device");//find_type_by_name() can be found in sysfs_helper.c
     if (dev_num < 0) {
         DEBUG_MSG("Failed to find the %s\n", device_name);
         ret = -ENODEV;
         goto error_ret;
     }
-//    DEBUG_MSG("iio device number being used is %d\n", dev_num);
 
+//    DEBUG_MSG("iio device number being used is %d\n", dev_num);
     asprintf(&dev_dir_name, "%siio:device%d", iio_dir, dev_num);
     if (trigger_name == NULL) {
+
+
         /*
-         * Build the trigger name. If it is device associated it's
+         * Build the trigger name. If it is device associated its
          * name is <device_name>_dev[n] where n matches the device
          * number found above
          */
     	asret = asprintf(&trigger_name, "%s-dev%d", device_name, dev_num);
         if (asret < 0) {
-            ret = -ENOMEM;
+            ret = -ENOMEM;	//ENOMEM = Error: NO MEMory
             goto error_ret;
         }
     }
@@ -305,7 +322,7 @@ int discover (void)
     trig_num = find_type_by_name(trigger_name, "trigger");
     if (trig_num < 0) {
         DEBUG_MSG("Failed to find the trigger %s\n", trigger_name);
-        ret = -ENODEV;
+        ret = -ENODEV;	//ENODEV = Error: NO DEVice
         goto error_ret;
     }
 //    DEBUG_MSG("iio trigger number being used is %d\n", trig_num);
@@ -328,16 +345,17 @@ int main(void)
 	  goto error_exit;
 
   err = pthread_create(&agread_id, NULL, &ag_read_thread, NULL);
-  err = err;
+  err = err;		//If you comment this out, you get a compiler warning. Otherwise, see reddit.com/r/badcode
 
   reader_runnning = true;
-  
+
+  //I don't think this does anything
   while(reader_runnning){
 	  usleep(200000);
   }
-  
+
   if (agread_id != 0) {
-      pthread_join(agread_id, (void**) &b);  
+      pthread_join(agread_id, (void**) &b);
   }
 
  error_exit:

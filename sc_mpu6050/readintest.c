c7145f45483de4a76d06113eedc2e6c0243c5f17
Bretz <spencer.bretz@undcemlh110-04l.ad.und.edu>
2019-04-17 18:22:49 -0500

    readintest added

--- sc_mpu6050/readintest.c
+++ sc_mpu6050/readintest.c
@@ -8,4 +8,13 @@
 #include "sysfs_helper.h" //Identifies chips, helps out in general
 #include "types.h"        // 
 
-const char* iio_dir = "/sys/bus/iio/devices/";
+const char* y_dir = "/sys/bus/iio/devices/iio:device1/in_accel_y_raw";	//Y accelerometer location
+void main(void)
+{
+	long num;
+	char* line;
+	FILE* file = fopen(y_dir);
+	fscanf(*file, line);
+	num = atol(line);
+	printf("%ld\n",line);
+}
